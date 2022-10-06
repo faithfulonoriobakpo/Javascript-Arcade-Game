ba082945c7d3cae22c28206f8059e38a4baafc6b
Faithful Onoriobakpo <P9390@fidelitybank.ng>
2022-10-06 16:27:27 +0100

    Add Player Class

--- js/app.js
+++ js/app.js
@@ -21,6 +21,33 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+class Player {
+    constructor(x=200,y=380){
+        this.sprite = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
+    }
+    handleInput(direction){
+        if(direction == 'left' && this.x != 0){
+            this.x -= 100;
+        }
+        else if(direction == 'right' && this.x != 400){
+            this.x += 100;
+        }
+        else if(direction == 'up' && this.y != -20){
+            this.y -= 80;
+        }
+        else if(direction == 'down' && this.y != 380){
+            this.y += 80;
+        }
+    }
+    update(){
+
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // This listens for key presses and sends the keys to
 // Player.handleInput() method.
